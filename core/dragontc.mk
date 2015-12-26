9e0fdda4292deca1b91d2ed578f5207a91eaff7c
Joe Maples <frap129@gmail.com>
2015-12-25 22:33:45 -0500

    tfw when you forget the most important file
    Signed-off-by: Joe Maples <frap129@gmail.com>

---
+++ core/dragontc.mk
@@ -0,0 +1,58 @@
+# Copyright (C) 2015 DragonTC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Polly flags for use with Clang
+POLLY := -mllvm -polly \
+         -mllvm -polly-parallel -lgomp \
+         -mllvm -polly-parallel-force \
+	 -mllvm -polly-allow-nonaffine=1\
+         -mllvm -polly-ast-detect-parallel \
+         -mllvm -polly-no-early-exit \
+         -mllvm -polly-vectorizer=stripmine \
+         -mllvm -polly-opt-fusion=max \
+         -mllvm -polly-opt-maximize-bands=yes
+
+DISABLE_POLLY_TARGET := \
+		$(LOCAL_DISABLE_POLLY_TARGET) \
+
+DISABLE_POLLY_HOST := \
+                $(LOCAL_DISABLE_POLLY_HOST) \
+		libicuuc-host_32 \
+		libjavacore 
+
+# Check if we should enable Polly on target modules.
+ifneq (,$(filter true,$(LOCAL_CLANG)))
+  ifeq (,$(filter true,$(LOCAL_IS_HOST_MODULE)))
+    ifneq (1,$(words $(filter $(DISABLE_POLLY_TARGET),$(LOCAL_MODULE))))
+      ifdef LOCAL_CFLAGS
+        LOCAL_CFLAGS += $(POLLY)
+      else
+        LOCAL_CFLAGS := $(POLLY)
+      endif
+    endif
+  endif
+endif
+
+# Check if we should enable Polly on host modules.
+ifneq (,$(filter true,$(LOCAL_CLANG)))
+  ifneq (,$(filter true,$(LOCAL_IS_HOST_MODULE)))
+    ifneq (1,$(words $(filter $(DISABLE_POLLY_HOST),$(LOCAL_MODULE))))
+      ifdef LOCAL_CFLAGS
+        LOCAL_CFLAGS += $(POLLY)
+      else
+        LOCAL_CFLAGS := $(POLLY)
+      endif
+    endif
+  endif
+endif
